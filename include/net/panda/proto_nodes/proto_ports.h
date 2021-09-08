8a25d1b1480d12f07819967ec0165ed8f664b47e
Felipe Magno de Almeida <felipe@sipanda.io>
2021-09-08 17:57:53 -0300

    net: Add PANDA network packet parser
    Adds the generic PANDA parser implementation to net/panda and the
    auxiliary include headers that are used by the optimized and generic
    implementation to include/net/panda.
    
    The PANDA project allows the user to to create network packet parsers
    by combining and matching protocols in a declarative way, albeit in C
    code. This feature allows the flexibility users need to write parsers
    for specific protocols or to change the specifics on how and what data
    to extract from network packets.
    
    The PANDA project also has a C-to-C compiler that reads a C file with
    a declarative parser definition and outputs an optimized version that
    uses the compiler inline optimizations to achieve much better
    performance than a generic implementation could. This compiler could
    be added later to the tools directory to be used in other subsystems
    to generate optimized versions of specialized parsers.
    
    A couple drawbacks of this commit (which is meant as a RFC PATCH) is
    that it fails checkpatch in some cases and that it has some code
    repetition from existing code in kernel. Keep in mind that these
    drawbacks are known and they will get fixed with a proper submission
    patch.
    
    For example, to define a very simple parser, you would define the
    parser nodes and a parser table for each non-terminal parser as such:
    
    ```
    PANDA_MAKE_PARSE_NODE(ether_node, panda_parse_ether, NULL, NULL, ether_table);
    PANDA_MAKE_PARSE_NODE(ipv4_node, panda_parse_ipv4, extract_ipv4, NULL,
    		      ip_table);
    PANDA_MAKE_PARSE_NODE(ipv6_node, panda_parse_ipv6, extract_ipv6, NULL,
    		      ip_table);
    PANDA_MAKE_LEAF_PARSE_NODE(ports_node, panda_parse_ports, extract_ports, NULL);
    
    PANDA_MAKE_PROTO_TABLE(ether_table,
    	{ __cpu_to_be16(ETH_P_IP), &ipv4_node },
    	{ __cpu_to_be16(ETH_P_IPV6), &ipv6_node },
    );
    
    PANDA_MAKE_PROTO_TABLE(ip_table,
    	{ IPPROTO_TCP, &ports_node },
    	{ IPPROTO_UDP, &ports_node },
    );
    
    PANDA_PARSER(parser, "Simple parser without md templates", &ether_node);
    ```
    
    Which you can run this way:
    
    ```
    panda_parse(parser, packet, len, &extracted_header_metadata, 0, 0);
    ```
    
    Signed-off-by: Felipe Magno de Almeida <felipe@sipanda.io>

---
+++ include/net/panda/proto_nodes/proto_ports.h
@@ -0,0 +1,59 @@
+/* SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2020,2021 SiPanda Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef __PANDA_PROTO_PORTS_H__
+#define __PANDA_PROTO_PORTS_H__
+
+#include "net/panda/parser.h"
+
+/* Transport nodes with ports definitions */
+
+struct port_hdr {
+	union {
+		__be32   ports;
+		struct {
+			__be16 sport;
+			__be16 dport;
+		};
+	};
+};
+
+#endif /* __PANDA_PROTO_PORTS_H__ */
+
+#ifdef PANDA_DEFINE_PARSE_NODE
+
+/* Generic PANDA parse nodes for transport protocols that contain port
+ * numbers cnanonical location
+ *
+ * Transport header starts with sixteen bit source and destination port
+ * numbers. Applicable protocols include TCP, UDP, SCTP, etc.
+ */
+static const struct panda_proto_node panda_parse_ports __unused() = {
+	.name = "Transport with ports",
+	.min_len = sizeof(struct port_hdr),
+};
+
+#endif /* PANDA_DEFINE_PARSE_NODE */
