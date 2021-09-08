4b5cd17c63463076e31d9170e53bb8f0acca5aca
Felipe Magno de Almeida <felipe@sipanda.io>
2021-09-08 17:57:53 -0300

    net: Add PANDA network packet parser
    Adds the generic PANDA parser implementation to net/panda and the
    auxiliary include headers that are used by the optimized and generic
    implementation to include/net/panda.
    
    The PANDA project allows the user to to create network packet parsers
    by combining and matching protocols in a declarative way, albeit in C
    code. This feature allows the flexibility users need to write parsers
    for specific protocols or to change the specifics on how and what data
    to extract from network packets.
    
    The PANDA project also has a C-to-C compiler that reads a C file with
    a declarative parser definition and outputs an optimized version that
    uses the compiler inline optimizations to achieve much better
    performance than a generic implementation could. This compiler could
    be added later to the tools directory to be used in other subsystems
    to generate optimized versions of specialized parsers.
    
    A couple drawbacks of this commit (which is meant as a RFC PATCH) is
    that it fails checkpatch in some cases and that it has some code
    repetition from existing code in kernel. Keep in mind that these
    drawbacks are known and they will get fixed with a proper submission
    patch.
    
    For example, to define a very simple parser, you would define the
    parser nodes and a parser table for each non-terminal parser as such:
    
    ```
    PANDA_MAKE_PARSE_NODE(ether_node, panda_parse_ether, NULL, NULL, ether_table);
    PANDA_MAKE_PARSE_NODE(ipv4_node, panda_parse_ipv4, extract_ipv4, NULL,
    		      ip_table);
    PANDA_MAKE_PARSE_NODE(ipv6_node, panda_parse_ipv6, extract_ipv6, NULL,
    		      ip_table);
    PANDA_MAKE_LEAF_PARSE_NODE(ports_node, panda_parse_ports, extract_ports, NULL);
    
    PANDA_MAKE_PROTO_TABLE(ether_table,
    	{ __cpu_to_be16(ETH_P_IP), &ipv4_node },
    	{ __cpu_to_be16(ETH_P_IPV6), &ipv6_node },
    );
    
    PANDA_MAKE_PROTO_TABLE(ip_table,
    	{ IPPROTO_TCP, &ports_node },
    	{ IPPROTO_UDP, &ports_node },
    );
    
    PANDA_PARSER(parser, "Simple parser without md templates", &ether_node);
    ```
    
    Which you can run this way:
    
    ```
    panda_parse(parser, packet, len, &extracted_header_metadata, 0, 0);
    ```
    
    Signed-off-by: Felipe Magno de Almeida <felipe@sipanda.io>

---
+++ include/net/panda/proto_nodes/proto_gre.h
@@ -0,0 +1,290 @@
+/* SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2020,2021 SiPanda Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef __PANDA_PROTO_GRE_H__
+#define __PANDA_PROTO_GRE_H__
+
+/* GRE node definitions */
+
+#ifndef __KERNEL__
+#include <arpa/inet.h>
+#endif
+
+#include <linux/ip.h>
+
+#include "net/panda/parser.h"
+
+/* Define common GRE constants. These normally come from linux/if_tunnel.h,
+ * however that include file has a lot of other definitions beyond just GRE
+ * which can be a problem compiling especially with older kernel includes. So
+ * we define the GRE values here if they aren't already defined elsewhere.
+ */
+#ifndef GRE_CSUM
+#define GRE_CSUM	__cpu_to_be16(0x8000)
+#endif
+
+#ifndef GRE_ROUTING
+#define GRE_ROUTING	__cpu_to_be16(0x4000)
+#endif
+
+#ifndef GRE_KEY
+#define GRE_KEY		__cpu_to_be16(0x2000)
+#endif
+
+#ifndef GRE_SEQ
+#define GRE_SEQ		__cpu_to_be16(0x1000)
+#endif
+
+#ifndef GRE_ACK
+#define GRE_ACK		__cpu_to_be16(0x0080)
+#endif
+
+#ifndef GRE_VERSION
+#define GRE_VERSION	__cpu_to_be16(0x0007)
+#endif
+
+#ifndef GRE_VERSION_0
+#define GRE_VERSION_0	__cpu_to_be16(0x0000)
+#endif
+
+#ifndef GRE_VERSION_1
+#define GRE_VERSION_1	__cpu_to_be16(0x0001)
+#endif
+
+#ifndef GRE_PROTO_PPP
+#define GRE_PROTO_PPP	__cpu_to_be16(0x880b)
+#endif
+
+#ifndef GRE_PPTP_KEY_MASK
+#define GRE_PPTP_KEY_MASK	__cpu_to_be32(0xffff)
+#endif
+
+/* GRE flag-field definitions */
+static const struct panda_flag_fields gre_flag_fields = {
+	.fields = {
+		{
+#define GRE_FLAGS_CSUM_IDX	0
+			.flag = GRE_CSUM,
+			.size = sizeof(__be32),
+		},
+		{
+#define GRE_FLAGS_KEY_IDX	1
+			.flag = GRE_KEY,
+			.size = sizeof(__be32),
+		},
+#define GRE_FLAGS_SEQ_IDX	2
+		{
+			.flag = GRE_SEQ,
+			.size = sizeof(__be32),
+		},
+#define GRE_FLAGS_NUM_IDX	3
+	},
+	.num_idx = GRE_FLAGS_NUM_IDX
+};
+
+#define GRE_FLAGS_V0_MASK	(GRE_CSUM | GRE_KEY | GRE_SEQ | GRE_ROUTING)
+
+static const struct panda_flag_fields pptp_gre_flag_fields = {
+	.fields = {
+		{
+#define GRE_PPTP_FLAGS_CSUM_IDX	0
+			.flag = GRE_CSUM,
+			.size = sizeof(__be32),
+		},
+		{
+#define GRE_PPTP_FLAGS_KEY_IDX	1
+			.flag = GRE_KEY,
+			.size = sizeof(__be32),
+		},
+#define GRE_PPTP_FLAGS_SEQ_IDX	2
+		{
+			.flag = GRE_SEQ,
+			.size = sizeof(__be32),
+		},
+#define GRE_PPTP_FLAGS_ACK_IDX	3
+		{
+			.flag = GRE_ACK,
+			.size = sizeof(__be32),
+		},
+#define GRE_PPTP_FLAGS_NUM_IDX	4
+	},
+	.num_idx = GRE_PPTP_FLAGS_NUM_IDX
+};
+
+#define GRE_FLAGS_V1_MASK	(GRE_CSUM | GRE_KEY | GRE_SEQ |		\
+				 GRE_ROUTING | GRE_ACK)
+
+#define GRE_PPTP_KEY_MASK	__cpu_to_be32(0xffff)
+
+struct gre_hdr {
+	__be16 flags;
+	__be16 protocol;
+	__u8 fields[0];
+};
+
+struct panda_pptp_id {
+	union {
+		struct {
+			__u16 payload_len;
+			__u16 call_id;
+		};
+		__u32 val32;
+	};
+};
+
+static inline int gre_proto_version(const void *vgre)
+{
+	return ntohs(((struct gre_hdr *)vgre)->flags & GRE_VERSION);
+}
+
+static inline size_t gre_v0_len_from_flags(unsigned int flags)
+{
+	return sizeof(struct gre_hdr) +
+		panda_flag_fields_length(flags, &gre_flag_fields);
+}
+
+static inline ssize_t gre_v0_len_check(const void *vgre)
+{
+	const struct gre_hdr *gre = vgre;
+
+	/* Check for valid set of flags */
+	if (panda_flag_fields_check_invalid(gre->flags, GRE_FLAGS_V0_MASK |
+								GRE_VERSION))
+		return PANDA_STOP_BAD_FLAG;
+
+	/* Only look inside GRE without routing */
+	if (((struct gre_hdr *)vgre)->flags & GRE_ROUTING)
+		return PANDA_STOP_OKAY;
+
+	return gre_v0_len_from_flags(gre->flags);
+}
+
+static inline int gre_v0_proto(const void *vgre)
+{
+	return ((struct gre_hdr *)vgre)->protocol;
+}
+
+static inline size_t gre_v1_len_from_flags(unsigned int flags)
+{
+	return sizeof(struct gre_hdr) +
+		panda_flag_fields_length(flags, &pptp_gre_flag_fields);
+}
+
+static inline ssize_t gre_v1_len_check(const void *vgre)
+{
+	const struct gre_hdr *gre = vgre;
+
+	/* Check for valid set of flags */
+	if (panda_flag_fields_check_invalid(gre->flags, GRE_FLAGS_V1_MASK |
+								GRE_VERSION))
+		return PANDA_STOP_BAD_FLAG;
+
+	/* Only look inside GRE without routing */
+	if (((struct gre_hdr *)vgre)->flags & GRE_ROUTING)
+		return PANDA_STOP_OKAY;
+
+	/* Version1 must be PPTP, and check that keyid id set */
+	if (!(gre->protocol == GRE_PROTO_PPP && (gre->flags & GRE_KEY)))
+		return PANDA_STOP_OKAY;
+
+	return gre_v1_len_from_flags(gre->flags);
+}
+
+static inline int gre_v1_proto(const void *vgre)
+{
+	/* Protocol already checked in gre_v1_len_check. Returning zero
+	 * means PPP
+	 */
+
+	return 0;
+}
+
+#endif /* __PANDA_PROTO_GRE_H__ */
+
+#ifdef PANDA_DEFINE_PARSE_NODE
+
+/* panda_parse_gre_base protocol node
+ *
+ * Parse base GRE header as an overlay to determine GRE version
+ *
+ * Next protocol operation returns GRE version number (i.e. 0 or 1).
+ */
+static const struct panda_proto_node panda_parse_gre_base __unused() = {
+	.name = "GRE base",
+	.overlay = 1,
+	.min_len = sizeof(struct gre_hdr),
+	.ops.next_proto = gre_proto_version,
+};
+
+static inline __u32 gre_get_flags(const void *hdr)
+{
+	return ((struct gre_hdr *)hdr)->flags;
+}
+
+static inline size_t gre_fields_offset(const void *hdr)
+{
+	return sizeof(struct gre_hdr);
+}
+
+/* panda_parse_gre_v0 protocol node
+ *
+ * Parse a version 0 GRE header
+ *
+ * Next protocol operation returns a GRE protocol (e.g. ETH_P_IPV4).
+ */
+static const struct panda_proto_flag_fields_node
+					panda_parse_gre_v0 __unused() = {
+	.proto_node.node_type = PANDA_NODE_TYPE_FLAG_FIELDS,
+	.proto_node.name = "GRE v0",
+	.proto_node.encap = 1,
+	.proto_node.min_len = sizeof(struct gre_hdr),
+	.proto_node.ops.next_proto = gre_v0_proto,
+	.proto_node.ops.len = gre_v0_len_check,
+	.ops.get_flags = gre_get_flags,
+	.ops.start_fields_offset = gre_fields_offset,
+	.flag_fields = &gre_flag_fields,
+};
+
+/* panda_parse_gre_v1 protocol node
+ *
+ * Parse a version 1 GRE header
+ *
+ * Next protocol operation returns GRE_PROTO_PPP.
+ */
+static const struct panda_proto_flag_fields_node
+					panda_parse_gre_v1 __unused() = {
+	.proto_node.node_type = PANDA_NODE_TYPE_FLAG_FIELDS,
+	.proto_node.name = "GRE v1 - pptp",
+	.proto_node.encap = 1,
+	.proto_node.min_len = sizeof(struct gre_hdr),
+	.proto_node.ops.next_proto = gre_v1_proto,
+	.proto_node.ops.len = gre_v1_len_check,
+	.ops.get_flags = gre_get_flags,
+	.ops.start_fields_offset = gre_fields_offset,
+	.flag_fields = &pptp_gre_flag_fields,
+};
+
+#endif /* PANDA_DEFINE_PARSE_NODE */
